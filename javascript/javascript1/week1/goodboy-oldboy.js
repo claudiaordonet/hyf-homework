11a2640e0442545fa269dd7f2a750ec8e26eeee2
claudiaordonet <claudiaordonet@gmail.com>
2022-10-20 10:59:25 +0200

    dog age calculation exerc.2 javascript week1

---
+++ javascript/javascript1/week1/goodboy-oldboy.js
@@ -0,0 +1,18 @@
+// Goodboy-Oldboy (A dog age calculator)
+
+const dogYearOfBirth = 2017;
+let dogYearFuture = 2027;
+let dogYear = dogYearFuture - dogYearOfBirth;
+console.log(dogYear);
+
+let shouldShowResultInDogYears = true;
+
+if (shouldShowResultInDogYears == true) {
+  shouldShowResultInDogYears = dogYear * 7;
+} else {
+  dogYear;
+}
+
+console.log(
+  `Your dog will be ${dogYear} human years old in ${dogYearFuture} or Your dog will be ${shouldShowResultInDogYears} dog years old in ${dogYearFuture}`
+);
