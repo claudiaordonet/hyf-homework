8535ad14290e1302091ed3d5d239fbd9b64b6493
claudiaordonet <claudiaordonet@gmail.com>
2022-10-20 11:01:30 +0200

    house price calculation exer.3 javascript week1

---
+++ javascript/javascript1/week1/houseprice.js
@@ -0,0 +1,33 @@
+//Housey pricey (A house price estimator)
+
+let houseWidth = 8;
+let houseDeep = 10;
+let houseHigh = 10;
+
+let volumeInMeters = houseWidth * houseDeep * houseHigh;
+let gardenSizeInM2 = 100;
+
+let housePricePeter = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+
+if (housePricePeter < 2500000) {
+  housePricePeter = "Peter overpaid";
+}
+
+console.log(housePricePeter);
+
+let houseWidth2 = 5;
+let houseDeep2 = 11;
+let houseHigh2 = 8;
+
+let volumeInMeters2 = houseWidth2 * houseDeep2 * houseHigh2;
+let gardenSizeInM2julia = 70;
+
+let housePricejulia = volumeInMeters2 * 2.5 * 1000 + gardenSizeInM2julia * 300;
+
+if (housePricejulia > 1000000) {
+  housePricejulia = "Julia paid a good price";
+} else {
+  housePricejulia = "Julia overpaid";
+}
+
+console.log(housePricejulia);
