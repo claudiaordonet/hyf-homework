c79d414ab2aac7b2cc587c92d77d869eaae884ab
claudiaordonet <claudiaordonet@gmail.com>
2022-10-20 10:57:48 +0200

    first exercise age calculator javascript week1

---
+++ javascript/javascript1/week1/homework1.js
@@ -0,0 +1,9 @@
+//Age-ify (A future age calculator)
+
+const yearOfBirth = 1981;
+let yearFuture = 2050;
+
+let age = yearFuture - yearOfBirth;
+console.log(age);
+
+console.log(`You will be ${age} years old in ${yearFuture}`);
