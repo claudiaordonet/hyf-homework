f32e245e2f802c15db2f9bd9784c52af4d207c05
claudiaordonet <claudiaordonet@gmail.com>
2022-10-28 15:44:56 +0200

    how to print name and lastname in a ticket

---
+++ javascript/javascript1/week2/smart-ease.js
@@ -0,0 +1,12 @@
+function getFullname(firstname, surname) {
+  return firstname + surname;
+}
+
+const fullname = getFullname("Benjamin", "Hughes");
+console.log(fullname);
+
+const fullname1 = getFullname("Maria", "Smith");
+console.log(fullname1);
+
+const fullname2 = getFullname("Luciana", "Parker");
+console.log(fullname2);
